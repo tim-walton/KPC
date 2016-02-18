61d8482421ea69caff8bfeed6f5c33987e3841f2
Tim Walton <timdwalton@gmail.com>
2016-02-17 22:21:37 -0600

    test

--- resources/views/kpc.blade.php
+++ resources/views/kpc.blade.php
@@ -16,14 +16,9 @@
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-           <span class="nav-brand"><img src="images/kpc.jpg" height="32px" width="64px"> </span> <a class="navbar-brand" href="index.html">Kansas Pathology Consultants P.A.</a>
-        </div>
+
+           <span class="nav-brand"><img src="images/kpc.jpg" height="64px" width="96px"> <a class="navbar-brand" href="index.html">Kansas Pathology Consultants P.A.</a>
+        </div></span>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right">
