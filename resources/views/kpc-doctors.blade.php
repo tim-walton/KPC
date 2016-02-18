15684da330aad0619e7a3b4ce47b32ca2f45017a
Tim Walton <timdwalton@gmail.com>
2016-02-17 22:43:54 -0600

    test

--- resources/views/kpc-doctors.blade.php
+++ resources/views/kpc-doctors.blade.php
@@ -3,8 +3,8 @@
 <head>
     <title>Kansas Pathology Consultants</title>
 
-    <link href="css/app.css" rel="stylesheet" type="text/css">
-    <link href="css/custom.css" rel="stylesheet" type="text/css">
+    <link href="../css/app.css" rel="stylesheet" type="text/css">
+    <link href="../css/custom.css" rel="stylesheet" type="text/css">
 
     <style>
     </style>
