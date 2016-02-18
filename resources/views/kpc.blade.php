7b2d8b482fc9e32e331c011941bc4a14da6d4a09
Tim Walton <timdwalton@gmail.com>
2016-02-17 21:40:40 -0600

    test

--- resources/views/kpc.blade.php
+++ resources/views/kpc.blade.php
@@ -30,15 +30,51 @@
                 <li>
                     <a href="about.html">About</a>
                 </li>
-                <li>
-                    <a href="services.html">Services</a>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="full-width.html">Full Width Page</a>
+                        </li>
+                        <li>
+                            <a href="sidebar.html">Sidebar Page</a>
+                        </li>
+                        <li>
+                            <a href="faq.html">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="404.html">404</a>
+                        </li>
+                        <li>
+                            <a href="pricing.html">Pricing Table</a>
+                        </li>
+                    </ul>
                 </li>
                 <li>
                     <a href="contact.html">Contact</a>
                 </li>
 
 
-
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Our Staff <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="full-width.html">Full Width Page</a>
+                        </li>
+                        <li>
+                            <a href="sidebar.html">Sidebar Page</a>
+                        </li>
+                        <li>
+                            <a href="faq.html">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="404.html">404</a>
+                        </li>
+                        <li>
+                            <a href="pricing.html">Pricing Table</a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </div>
         <!-- /.navbar-collapse -->
