35ca105579f470aace0d236000f35540bf6f58c1
tim-walton <14597q30>
2016-02-22 10:26:25 -0600

    added cbq

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,7 +6,7 @@ Route::group(['middleware' => ['web']], function () {
     Route::get('/kpc/doctors', function(){
         return view('kpc-doctors');
     });
-    Route::get('/cbq/test/1', function() {
+    Route::get('/cbq/test/1', 'cbq@testCBQ', function() {
 
     });
 });
