caf792d09684adbc4ca55b16c5e0ab1c36abbf51
Tim Walton <timdwalton@gmail.com>
2016-02-22 20:54:48 -0600

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,24 +21,14 @@
                     We strive for rapid turnaround time, quality reports with a personal touch. Our pathologists are available to answer your questions and meet any special request you may have.</p>
                 <hr>
                 <p>We are a professional association of Board Certified Anatomic and Clinical Pathologists, Cytopathologists, Radioisotopic Pathologist and Cytotechnologists with a dedicated support staff.
-<br><br>
+<br><img src="images/cap.jpg"><br>
                     We are a Physician owned multi-state provider of Medical Pathology Services. We have maintained continuous voluntary accreditation with the College of American Pathologists since 1970. Our pathologists are all Board Certified and/or are Fellows of the College of American Pathologists (FCAP). We subscribe to ongoing self-inspection and ongoing educational programs in the realm of Surgical Pathology, Cytopathology and Clinical Consultative Services in Pathology. We are a member laboratory of the American Society of Cytopathology. Currently we hold physician licenses in Kansas, Nebraska, Colorado, Tennessee, Ohio, Illinois, New Mexico, Oklahoma, Texas. Our CLIA license is continuously maintained.
 <br><br>
                     Our physician staff includes Megan P. Redelman, M.D., FCAP, who serves as our Medical Director, Debra K. Barns, M.D., Lyle J. Noordhoek, M.D., FCAP, our Staff Pathologists; and Ward M. Newcomb, M.D., FCAP, Staff Pathologist and President of Kansas Pathology Consultants, P.A. Our support staff is educated, experienced and capable, assisting the physicians and cytotechnologists to consistently produce quality services and reliable work products, on time and at a reasonable fee.
 <br><br>
                     "The performance of one affects the performance of all."  Everyone at KPC works toward the goal of improving the products and services we provide to you and to the patients we serve. </p>
             </div><!-- /.blog-post -->
-            <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-                <div class="sidebar-module sidebar-module-inset">
-                    <img src="images/cap.jpg">
-                </div>
-                <div class="sidebar-module">
 
-                </div>
-                <div class="sidebar-module">
-
-                </div>
-            </div>
 <hr>
             <div class="blog-post">
                <img src="images/cars2.jpg" class="img-thumbnail">
