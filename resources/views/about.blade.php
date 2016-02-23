f20f9d83b9c78e82cefe96abafc82773c278ddc4
Tim Walton <timdwalton@gmail.com>
2016-02-22 20:45:02 -0600

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -45,7 +45,15 @@
                 </div>
             </div>
 
-    </div><!-- /.row -->
+    </div><!-- /.row --> <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </footer>
+
 
 </div>
 <!-- /.container -->
