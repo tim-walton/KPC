9c0a9cfe0fa947bf53b91729c1c210a9f2ae9ea7
Tim Walton <timdwalton@gmail.com>
2016-02-22 20:40:08 -0600

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,31 +20,13 @@
 
                     We strive for rapid turnaround time, quality reports with a personal touch. Our pathologists are available to answer your questions and meet any special request you may have.</p>
                 <hr>
-                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <h2>Heading</h2>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <pre><code>Example code block</code></pre>
-                <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <ul>
-                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                    <li>Donec id elit non mi porta gravida at eget metus.</li>
-                    <li>Nulla vitae elit libero, a pharetra augue.</li>
-                </ul>
-                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                <ol>
-                    <li>Vestibulum id ligula porta felis euismod semper.</li>
-                    <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                    <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-                </ol>
-                <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
+                <p>We are a professional association of Board Certified Anatomic and Clinical Pathologists, Cytopathologists, Radioisotopic Pathologist and Cytotechnologists with a dedicated support staff.
+<br>
+                    We are a Physician owned multi-state provider of Medical Pathology Services. We have maintained continuous voluntary accreditation with the College of American Pathologists since 1970. Our pathologists are all Board Certified and/or are Fellows of the College of American Pathologists (FCAP). We subscribe to ongoing self-inspection and ongoing educational programs in the realm of Surgical Pathology, Cytopathology and Clinical Consultative Services in Pathology. We are a member laboratory of the American Society of Cytopathology. Currently we hold physician licenses in Kansas, Nebraska, Colorado, Tennessee, Ohio, Illinois, New Mexico, Oklahoma, Texas. Our CLIA license is continuously maintained.
+<br>
+                    Our physician staff includes Megan P. Redelman, M.D., FCAP, who serves as our Medical Director, Debra K. Barns, M.D., Lyle J. Noordhoek, M.D., FCAP, our Staff Pathologists; and Ward M. Newcomb, M.D., FCAP, Staff Pathologist and President of Kansas Pathology Consultants, P.A. Our support staff is educated, experienced and capable, assisting the physicians and cytotechnologists to consistently produce quality services and reliable work products, on time and at a reasonable fee.
+<br>
+                    "The performance of one affects the performance of all."  Everyone at KPC works toward the goal of improving the products and services we provide to you and to the patients we serve. </p>
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
