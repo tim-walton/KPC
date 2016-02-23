c51cf67af1194acabc4a9adf3114f1c2a0558327
Tim Walton <timdwalton@gmail.com>
2016-02-22 21:08:00 -0600

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -22,12 +22,13 @@
                 <hr>
                 <h4>We are a professional association of Board Certified Anatomic and Clinical Pathologists, Cytopathologists, Radioisotopic Pathologist and Cytotechnologists with a dedicated support staff.</h4>
 <p class="text-center"><img src="images/cap.jpg" class="img-thumbnail"></p>
+                <div class="col-xs-12 text-center">
                     We are a Physician owned multi-state provider of Medical Pathology Services. We have maintained continuous voluntary accreditation with the College of American Pathologists since 1970. Our pathologists are all Board Certified and/or are Fellows of the College of American Pathologists (FCAP). We subscribe to ongoing self-inspection and ongoing educational programs in the realm of Surgical Pathology, Cytopathology and Clinical Consultative Services in Pathology. We are a member laboratory of the American Society of Cytopathology. Currently we hold physician licenses in Kansas, Nebraska, Colorado, Tennessee, Ohio, Illinois, New Mexico, Oklahoma, Texas. Our CLIA license is continuously maintained.
 <br><br>
                     Our physician staff includes Megan P. Redelman, M.D., FCAP, who serves as our Medical Director, Debra K. Barns, M.D., Lyle J. Noordhoek, M.D., FCAP, our Staff Pathologists; and Ward M. Newcomb, M.D., FCAP, Staff Pathologist and President of Kansas Pathology Consultants, P.A. Our support staff is educated, experienced and capable, assisting the physicians and cytotechnologists to consistently produce quality services and reliable work products, on time and at a reasonable fee.
 <br><br>
                     "The performance of one affects the performance of all."  Everyone at KPC works toward the goal of improving the products and services we provide to you and to the patients we serve. </p>
-            </div><!-- /.blog-post -->
+            </div></div><!-- /.blog-post -->
 
 <hr>
             <div class="blog-post">
