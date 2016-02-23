356ec84dd4cfd50a90272fe35b0316c6314003c4
Tim Walton <timdwalton@gmail.com>
2016-02-22 20:38:20 -0600

    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,10 +11,14 @@
         <div class="col-sm-8 blog-main">
 
             <div class="blog-post">
-                <h2 class="blog-post-title">Sample blog post</h2>
-                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+                <h2 class="blog-post-title">A litte more about us..</h2>
 
-                <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+
+                <p>We provide a full range of services to rural hospitals, surgery centers, physician practices, and specialty clinics in Kansas, Colorado, and surrounding areas.
+                    <br>
+                    As a physician owned practice, we are dedicated to providing our colleagues with superior services.
+                    <br>
+                    We strive for rapid turnaround time, quality reports with a personal touch. Our pathologists are available to answer your questions and meet any special request you may have.</p>
                 <hr>
                 <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
                 <blockquote>
