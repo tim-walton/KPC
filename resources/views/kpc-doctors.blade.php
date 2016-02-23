4ba66c2bac9df8c1c67b714e2ab4fbfec361465e
Tim Walton <timdwalton@gmail.com>
2016-02-22 20:02:27 -0600

    test

--- resources/views/kpc-doctors.blade.php
+++ resources/views/kpc-doctors.blade.php
@@ -102,17 +102,17 @@
                 <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
             </div><!--/.col-xs-6.col-lg-4-->
             <div class="col-xs-6 col-lg-4">
-                <h2>Ward M. Newcomb, M.D., FCAP</h2>
+                <h3>Ward M. Newcomb, M.D., FCAP</h3>
+                <p><img src="images/1.jpg" class="img-thumbnail"></p>
+                <p> details  </p>
+            </div><!--/.col-xs-6.col-lg-4-->
+            <div class="col-xs-6 col-lg-4">
+                <h3>Debra K. Barns, M.D.</h3>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
                 <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
             </div><!--/.col-xs-6.col-lg-4-->
             <div class="col-xs-6 col-lg-4">
-                <h2>Debra K. Barns, M.D.</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
-            </div><!--/.col-xs-6.col-lg-4-->
-            <div class="col-xs-6 col-lg-4">
-                <h2>Lyle J. Noordhoek, M.D., FCAP</h2>
+                <h3>Lyle J. Noordhoek, M.D., FCAP</h3>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
                 <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
             </div><!--/.col-xs-6.col-lg-4-->
