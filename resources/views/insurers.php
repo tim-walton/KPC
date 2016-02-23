68d726723e486713f059b622d7392b0a7356b31d
Tim Walton <timdwalton@gmail.com>
2016-02-22 21:27:30 -0600

    test

---
+++ resources/views/insurers.php
@@ -0,0 +1,207 @@
+@extends('app')
+<!-- Header Carousel -->
+<header>
+</header>
+
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Marketing Icons Section -->
+
+    <!-- /.row -->
+    <!-- Portfolio Section -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Participating Insurers<br>
+                <small>Kansas Pathology Consultants P.A. works with the following Insurers to provide you service
+                </small>
+            </h2>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-lg-12 text-center">
+                <h3>Participating Insurers</h3>
+                <table class="table table-bordered table-responsive">
+                    <thead>
+                    <tr>
+                        <td>Insurer</td>
+                        <td>Coverage Provided</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+
+                    <tr>
+                        <td>American Healthcare Alliance Network</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Blue Cross/Blue Shield of KS, OK, NE, MO, CO
+                        </td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Blue Select-Premier Blue</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>ChampVA</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Choice Care Network</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Cigna (Preferred Health Care Network, Health Partners of KS Network)</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Colorado Migrant Program</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Early Detection Works (Cowley, Crawford, Rooks, and Sedgwick Counties)</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Every Woman Matters</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Fiserv</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>First Health</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>First Guard</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Great Rivers Network</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Health Partners of Kansas</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Healthlink Network</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Health Partners of Kansas</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td> Heartland Health</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>KS Breast and Cervical Cancer Initiative (KBCCI)</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>KS Health Plan</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Medicaid (KS, MO, AR, NE, and CO)</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Medicare</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Midlands Choice</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Molina Healthcare of New Mexico (formerly Cimarron Health Plan)</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Preferred Health Systems</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Preferred Plus of Kansas</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Principle Health Care</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Project Access</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Railroad Medicare (Travelers)</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Sloans Lake Managed Care</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Triwest</td>
+                        <td>Yes</td>
+                    </tr>
+                    <tr>
+                        <td>Wichita Preferred Provider Associates (WPPA Network)</td>
+                        <td>Yes</td>
+                    </tr>
+
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="well">
+        <div class="row">
+            <div class="col-md-8">
+                <p>Looking for our client login?</p>
+            </div>
+            <div class="col-md-4">
+                <a class="btn btn-lg btn-default btn-block" href="#">Login Here</a>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Your Website 2014</p>
+            </div>
+        </div>
+    </footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Script to Activate the Carousel -->
+<script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+</script>
+
+</body>
+</html>
